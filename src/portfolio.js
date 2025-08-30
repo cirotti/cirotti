2d549754a1e42d920e207e852e58e096e9706370
cirotti <cm718199@gmail.com>
2025-08-30 00:13:01 -0400

    Cambios1

--- src/portfolio.js
+++ src/portfolio.js
@@ -254,7 +254,7 @@ const achievementSection = {
       title: "Desarrollo Web",
       subtitle:
         "Creación de sitios web modernos, responsivos y optimizados con tecnologías actuales.",
-      image: require("./assets/images/codeInLogo.webp"),
+      image: require("./assets/images/21034578.jpg"),
       imageAlt: "Google Code-In Logo",
       footerLink: [
         {
