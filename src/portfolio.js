c274699510f258efd868b7229632f9bd14502e66
cirotti <cm718199@gmail.com>
2025-08-29 22:35:01 -0400

    Cambios5

--- src/portfolio.js
+++ src/portfolio.js
@@ -47,16 +47,15 @@ const socialMediaLinks = {
 // Skills Section
 
 const skillsSection = {
-  title: "Lo que hago",
-  subTitle: "DESARROLLADOR FULL STACK APASIONADO POR WEB, MÓVIL, DISEÑO Y BOTS",
+  title: "What I do",
+  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
   skills: [
     emoji(
-      "⚡ Desarrollo de páginas web altamente interactivas y aplicaciones móviles modernas"
+      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
     ),
-    emoji("⚡ Creación de bots para múltiples plataformas y automatización de tareas"),
+    emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
     emoji(
-      "⚡ Diseño gráfico y creación de interfaces atractivas y funcionales"
-      
+      "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
     )
   ],
 
