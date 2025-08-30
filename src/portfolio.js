d669b8e50e963cddeb8bd63c00b2a804e352dea5
cirotti <cm718199@gmail.com>
2025-08-29 22:01:40 -0400

    Cambios3

--- src/portfolio.js
+++ src/portfolio.js
@@ -33,13 +33,12 @@ const greeting = {
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
+  github: "https://github.com/",
+  linkedin: "https://www.linkedin.com/",
+  gmail: "cirottidev@gmail.com",
+  gitlab: "https://gitlab.com/cirotti",
   medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+  stackoverflow: "https://stackoverflow.com/users/31382362/cirotti",
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
