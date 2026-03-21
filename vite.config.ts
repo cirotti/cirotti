cd9914e7c03937525dab25c66fe479f3818c9314
cirotti <cm718199@gmail.com>
2026-03-21 19:47:04 -0300

    fix vite base for cloudflare pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,18 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+  base: "/",
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     host: "::",
     port: 8080,
     hmr: {
       overlay: false,
-    },
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
